8d8eb83063b2d89bbabd202cbf04890ce53825f5
Pratik Deshpande <aristopratt.work@gmail.com>
2020-09-15 15:15:55 +0530

    Java Generics Example

---
+++ CS5343-UTD/Projects/JavaReview/GenericExample.java
@@ -0,0 +1,21 @@
+class Container<AnyType extends Number>{
+    private AnyType value;
+
+    public AnyType getValue() {
+        return value;
+    }
+
+    public void setValue(AnyType value) {
+        this.value = value;
+    }
+}
+
+public class GenericExample {
+    public static void main(String[] args) {
+        System.out.println("Hi from Generics");
+        Container<Double> i = new Container<>();
+        i.setValue(10.10);
+        double x = i.getValue();
+        System.out.println(i.getValue() + x);
+    }
+}
