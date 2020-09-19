3042e05d43dc566d52a89d204ae06f780b78598e
Pratik Deshpande <aristopratt.work@gmail.com>
2020-09-19 12:56:53 +0530

    Stack class using ArrayList

---
+++ CS5343-UTD/Projects/Project1/MyStack.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MyStack<AnyType>{
+    private List<AnyType> list;
+
+    public MyStack(){
+        list = new ArrayList<AnyType>();
+    }
+    
+    public void push(AnyType val){
+        addToStack(val);
+    }
+
+    private void addToStack(AnyType val){
+        list.add(val);
+    }
+
+    public AnyType pop(){
+        return removeFromStack();
+    }
+
+    private AnyType removeFromStack(){
+        if(list.size() == 0){
+            throw new IndexOutOfBoundsException( "Stack is Empty");
+        }
+        else{
+            AnyType element = list.get(list.size() - 1);
+            list.remove(list.size()-1); 
+            return element;
+        }
+    }
+
+    public boolean isEmpty(){
+        return isListEmpty();
+    }
+
+    private boolean isListEmpty(){
+        if(list.size()==0){
+            return true;
+        }
+        return false;
+    }
+    
+}
