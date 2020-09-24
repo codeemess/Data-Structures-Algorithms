a1f7bfa11c1dd39cca4267b908ac3c86c8335d9b
Pratik Deshpande <aristopratt.work@gmail.com>
2020-09-24 19:43:56 +0530

    Solution for closest numbers on hacker rank.

---
+++ CompetitiveCode/closestNumbers.py
@@ -0,0 +1,17 @@
+numbers = [4,6,2,10,100]
+output = []
+numbers = sorted(numbers)
+nowmin = 10 ** 9
+    
+for ind in range(1, len(numbers)):
+    diff = abs(numbers[ind-1] - numbers[ind])
+        
+    if diff < nowmin:
+        output = [(numbers[ind-1], numbers[ind])]
+        nowmin = diff
+    elif diff == nowmin:
+        output.append((numbers[ind-1], numbers[ind]))
+            
+flat_list = [item for sublist in output for item in sublist]
+for i in range(0,len(flat_list),2):
+    print(numbers[i], numbers[i+1])
