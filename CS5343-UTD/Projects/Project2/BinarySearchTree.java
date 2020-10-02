388115a689b815ce079566346b6f198ca999eae0
Pratik Deshpande <aristopratt.work@gmail.com>
2020-10-02 14:52:29 +0530

    Node-count implemented.

--- CS5343-UTD/Projects/Project2/BinarySearchTree.java
+++ CS5343-UTD/Projects/Project2/BinarySearchTree.java
@@ -235,6 +235,22 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
             return 1 + Math.max( height( t.left ), height( t.right ) );    
     }
     
+    public int nodeCount(){
+        if (isEmpty())
+            return 0;
+        else return nodeCount(root);
+    }
+
+    private int nodeCount(BinaryNode<AnyType> t){
+        int count = 1;
+        if(t.left != null){
+            count += nodeCount(t.left);
+        }
+        if(t.right != null){
+            count += nodeCount(t.right);
+        }
+        return count;
+    }
     // Basic node stored in unbalanced binary search trees
     private static class BinaryNode<AnyType>
     {
@@ -265,30 +281,33 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
     public static void main( String [ ] args )
     {
         BinarySearchTree<Integer> t = new BinarySearchTree<>( );
-        final int NUMS = 4000;
-        final int GAP  =   37;
+        // final int NUMS = 4000;
+        // final int GAP  =   37;
 
-        System.out.println( "Checking... (no more output means success)" );
+        // System.out.println( "Checking... (no more output means success)" );
 
-        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-            t.insert( i );
+        // for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+        //     t.insert( i );
 
-        for( int i = 1; i < NUMS; i+= 2 )
-            t.remove( i );
-
-        if( NUMS < 40 )
-            t.printTree( );
-        if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
-            System.out.println( "FindMin or FindMax error!" );
-
-        for( int i = 2; i < NUMS; i+=2 )
-             if( !t.contains( i ) )
-                 System.out.println( "Find error1!" );
-
-        for( int i = 1; i < NUMS; i+=2 )
-        {
-            if( t.contains( i ) )
-                System.out.println( "Find error2!" );
+        
+        for(int i = 0; i<10;i++){
+            t.insert(i);
         }
+        // t.printTree();
+        System.out.println(t.nodeCount());
+        // if( NUMS < 40 )
+        //     t.printTree( );
+        // if( t.findMin( ) != 2 || t.findMax( ) != NUMS - 2 )
+        //     System.out.println( "FindMin or FindMax error!" );
+
+        // for( int i = 2; i < NUMS; i+=2 )
+        //      if( !t.contains( i ) )
+        //          System.out.println( "Find error1!" );
+
+        // for( int i = 1; i < NUMS; i+=2 )
+        // {
+        //     if( t.contains( i ) )
+        //         System.out.println( "Find error2!" );
+        // }
     }
 }
